8b86c8c1ed823579f6311753dbb98d24d2d65315
kang youngmin <kymtion@gmai.com>
2023-06-14 23:32:47 +0900

    DatePicker 설정

---
+++ dayMind/dayMind/Mission/TimeSettingView.swift
@@ -0,0 +1,20 @@
+//
+//  TimeSettingView.swift
+//  dayMind
+//
+//  Created by 강영민 on 2023/06/13.
+//
+
+import SwiftUI
+
+struct TimeSettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimeSettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimeSettingView()
+    }
+}
