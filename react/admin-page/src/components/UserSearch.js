8722b1b25e16cafc46ede4f69203c1728d492bf7
kang youngmin <kymtion@gmai.com>
2023-08-26 22:52:06 +0900

    웹 유저검색 페이지 수정완료

--- react/admin-page/src/components/UserSearch.js
+++ react/admin-page/src/components/UserSearch.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { collection, getDocs, query, where } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
 import { db } from '../firebase';
 
 const UserSearch = () => {
@@ -12,8 +11,8 @@ const UserSearch = () => {
   const [successfulRefunds, setSuccessfulRefunds] = useState(0);
   const [totalDeposit, setTotalDeposit] = useState(0);
   const [totalWithdrawal, setTotalWithdrawal] = useState(0); 
-  const auth = getAuth();
-  const userAuth = auth.currentUser;
+
+
 
   const handleSearch = async () => {
     // userId로 검색
@@ -27,17 +26,6 @@ const UserSearch = () => {
     const foundUser = userSnapshot.docs.map(doc => doc.data())[0];
     setUser(foundUser);
 
-    // userAuth에서 이메일을 가져옵니다.
-if (userAuth) {
-  const email = userAuth.email;
-  const lastSignInTime = new Date(userAuth.metadata.lastSignInTime).toLocaleString('ko-KR'); // 마지막 로그인 시간
-  const creationTime = new Date(userAuth.metadata.creationTime).toLocaleString('ko-KR'); // 계정 생성 시간
-  // foundUser 객체에 이메일을 추가합니다.
-  foundUser.email = email;
-  foundUser.lastSignInTime = lastSignInTime;
-  foundUser.creationTime = creationTime;
-}
-  
     if (foundUser) {
       const userId = foundUser.userId;
       const missionSnapshot = await getDocs(query(collection(db, 'missions'), where('userId', '==', userId)));
@@ -84,6 +72,14 @@ if (userAuth) {
       // 결과 저장
       setTotalDeposit(totalDepositAmount);
       setTotalWithdrawal(totalWithdrawalAmount);
+
+      // 미션 데이터와 거래 데이터를 시간 기준으로 내림차순 정렬
+      const sortedMissions = missionData.sort((a, b) => new Date(b.selectedTime2) - new Date(a.selectedTime2));
+      const sortedTransactions = transactionData.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+      setMissions(sortedMissions);
+      setTransactions(sortedTransactions);
+
     }
   };
   
@@ -98,24 +94,18 @@ if (userAuth) {
       {user && (
         <div style={{ width: '100%', textAlign: 'center' }}>
           <h2>사용자 정보</h2>
-          <table style={{ borderCollapse: 'collapse', width: '70%', margin: '0 auto' }}>
+          <table style={{ borderCollapse: 'collapse', width: '30%', margin: '0 auto' }}>
             <thead>
              <tr style={{ fontWeight: 'bold' }}>
                 <td>UID</td>
                 <td>닉네임</td>
-                <td>이메일</td>
-                <td>마지막 로그인</td>
-                <td>계정 생성일</td>
-                <td>잔액:</td>
+                <td>잔액</td>
               </tr>
             </thead>
             <tbody>
               <tr>
                 <td>{user.userId}</td>
                 <td>{user.nickname}</td>
-                <td>{user.email}</td>
-                <td>{user.lastSignInTime}</td>
-                <td>{user.creationTime}</td>
                 <td>{user.balance}원</td>
               </tr>
             </tbody>
@@ -138,6 +128,7 @@ if (userAuth) {
           <table style={{ borderCollapse: 'collapse', width: '100%' }}>
             <thead>
               <tr>
+                <th>번호</th>
                 <th>시작 시간</th>
                 <th>종료 시간</th>
                 <th>미션 타입</th>
@@ -148,6 +139,7 @@ if (userAuth) {
             <tbody>
               {missions.map((mission, index) => (
                 <tr key={index}>
+                  <td>{index + 1}</td>
                   <td>{new Date(mission.selectedTime1).toLocaleString()}</td>
                   <td>{new Date(mission.selectedTime2).toLocaleString()}</td>
                   <td>{mission.missionType}</td>
@@ -173,6 +165,7 @@ if (userAuth) {
           <table style={{ borderCollapse: 'collapse', width: '100%' }}>
             <thead>
               <tr>
+                <th>번호</th>
                 <th>거래 타입</th>
                 <th>금액</th>
                 <th>날짜</th>
@@ -181,6 +174,7 @@ if (userAuth) {
             <tbody>
               {transactions.map((transaction, index) => (
                 <tr key={index}>
+                  <td>{index + 1}</td>
                   <td>{transaction.type}</td>
                   <td>{transaction.amount}원</td>
                   <td>{new Date(transaction.date).toLocaleString()}</td>
